11532757716810a929c7f1c1b5121f1faf6f04ba
Akram Ouardas <a.ouardas@esi-sba.dz>
2020-08-24 02:49:53 +0100

    adding arguments to fonctions

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,9 @@
-print("hello world !")
+blockchain = ["test"]
+
+def add_value(value): 
+    blockchain.append([blockchain[-1],value])
+    print(blockchain)
+
+add_value(3)
+add_value(2)
+add_value(103)

730f99e2507e805a327f5a97f6cff127bb33c1af
Akram Ouardas <a.ouardas@esi-sba.dz>
2020-08-25 01:28:47 +0100

    module 2 finished

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,22 @@
-blockchain = ["test"]
+blockchain = []
 
-def add_value(value): 
-    blockchain.append([blockchain[-1],value])
-    print(blockchain)
 
-add_value(3)
-add_value(2)
-add_value(103)
+def get_last_blockchain_value():
+    """Returns the last value of the blockchain"""
+    return blockchain[-1]
+
+
+def add_value(value, last_transaction=[1]):
+    """Append a new value as the last value of the blockchain"""
+    blockchain.append([last_transaction, value])
+
+
+def get_user_input():
+    return float(input("enter your transaction amount :"))
+
+
+add_value(get_user_input())
+add_value(last_transaction=get_last_blockchain_value(),
+          value=get_user_input())
+add_value(get_user_input(), get_last_blockchain_value())
+print(blockchain)

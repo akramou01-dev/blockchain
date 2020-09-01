801ee3a3df9c7f6d2939dd75c6254db97a716c30
Akram Ouardas <a.ouardas@esi-sba.dz>
2020-09-01 02:19:51 +0100

    Adding the With statement

---
+++ files.py
@@ -0,0 +1,14 @@
+with open('demo.txt',mode='w') as f:
+
+    # # test = f.read()
+    # # f.write('this is a test \n abt the line break')
+    # file_content= f.readlines() 
+
+    # for line in file_content:
+    #     print(line[1])
+
+    # f.close()
+
+    f.write('Testing if this closes')
+
+print('Done')
